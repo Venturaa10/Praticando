653bb2369b83c624f0442e727dd1ce88cd45b3b8
Joao Victor Ventura <joao.victor.ventura25@gmail.com>
2024-05-01 18:25:05 -0300

    Praticando dicionario utilizando metodos, com exercicios basicos iniciais

--- dicionarios/dic01.py
+++ dicionarios/dic01.py
@@ -14,11 +14,63 @@ print(f'Frutas: {frutas}')
 # 2 - A partir do dicionário anterior, adicione uma nova fruta com sua quantidade.(FEITO)
 frutas['Morango'] = 50
 print(f'Frutas: {frutas}')
-# 3 - Escreva um código para verificar se uma determinada fruta está presente no dicionário.
+# 3 - Escreva um código para verificar se uma determinada fruta está presente no dicionário.(FEITO)
+nome_fruta = input('Nome da Fruta: ').capitalize().strip()
 
-# 4 - Escreva um programa para imprimir todas as chaves do dicionário.
-# 5 - Escreva um código para imprimir todos os valores do dicionário.
-# 6 - Escreva um programa para calcular o total de todas as quantidades de frutas no dicionário.
-# 7 - Escreva um código para encontrar a fruta com a maior quantidade no dicionário. Remova uma fruta do dicionário e imprima o dicionário atualizado.
-# Limpe o dicionário, removendo todos os itens.
-# Crie um novo dicionário contendo pelo menos três frutas diferentes e suas quantidades e, em seguida, combine esse dicionário com o primeiro dicionário criado.
+if nome_fruta in frutas:
+    print(f'A fruta {nome_fruta} está presente no dicionário')
+else:
+    print(f'A fruta {nome_fruta} não está presente no dicionário')
+
+print()
+# 4 - Escreva um programa para imprimir todas as chaves do dicionário.(FEITO)
+"""As duas maneiras que consegui"""
+print(frutas.keys())
+for chave in frutas.keys():
+    print(f'Chaves do dicionário Fruta: {chave}')
+
+print()
+
+# 5 - Escreva um código para imprimir todos os valores do dicionário.(FEITO)
+"""As duas maneiras que consegui"""
+print(frutas.values())
+for valor in frutas.values():
+    print(valor)
+print()
+# 6 - Escreva um programa para calcular o total de todas as quantidades de frutas no dicionário.(FEITO)
+print(f'Quantidades de unidades de frutas: {sum(frutas.values())}')
+
+# 7 - Escreva um código para encontrar a fruta com a maior quantidade no dicionário. 
+# Remova uma fruta do dicionário e imprima o dicionário atualizado.  (FEITO)
+# Limpe o dicionário, removendo todos os itens.(Feito)
+# Crie um novo dicionário contendo pelo menos três frutas diferentes e suas quantidades e, em seguida, combine esse dicionário com o primeiro dicionário criado.(FEITO)
+
+for fruta in frutas:
+    print(fruta)
+
+print(frutas.items())
+
+print(f'A fruta em maior quantidade no estoque contém: {max(frutas.values())} unidades.')
+print(f'Removendo fruta: {frutas.popitem()}\n') #Remove o ultimo item adicionado ao dicionario
+
+print(f'Removendo chave especifica: {frutas.pop('Pera')}') #Remove a chave especifica, e retorna o valor dessa chave
+print(f'Dicionario atualizado: {frutas}')
+print()
+
+frutas2 = {}
+frutas2['Abacaxi'] = 27
+frutas2['Laranja'] = 17
+frutas2['Goiaba'] = 15
+
+frutas.update(frutas2)
+print(f'Juntando os dois dicionarios de frutas: {frutas}')
+
+
+print()
+frutas.clear()
+if len(frutas) == 0:
+    print('Não há frutas!')
+else:
+    print(frutas)
+
+
