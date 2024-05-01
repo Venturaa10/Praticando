bf4498cf07289b75b7efe934a4f4a02150daa416
Joao Victor Ventura <joao.victor.ventura25@gmail.com>
2024-05-01 17:12:24 -0300

    Inicionando meus estudos sobre dicionarios

---
+++ dicionarios/dic01.py
@@ -0,0 +1,24 @@
+import os
+os.system('cls')
+# Sintaxe Basica: dicionario = {'chave': valor, 'chave2': valor2}
+
+# 1 - Crie um dicionário vazio chamado meu_dicionario e adicione três pares chave-valor a ele, representando nomes de frutas e suas quantidades. (FEITO)
+frutas = {}
+"""Sintaxe para adicionar chave e valor ao dicionario:
+dicionario['Chave'] = valor
+"""
+frutas['Banana'] = 20
+frutas['Pera'] = 30
+frutas['Maça'] = 10
+print(f'Frutas: {frutas}')
+# 2 - A partir do dicionário anterior, adicione uma nova fruta com sua quantidade.(FEITO)
+frutas['Morango'] = 50
+print(f'Frutas: {frutas}')
+# 3 - Escreva um código para verificar se uma determinada fruta está presente no dicionário.
+
+# 4 - Escreva um programa para imprimir todas as chaves do dicionário.
+# 5 - Escreva um código para imprimir todos os valores do dicionário.
+# 6 - Escreva um programa para calcular o total de todas as quantidades de frutas no dicionário.
+# 7 - Escreva um código para encontrar a fruta com a maior quantidade no dicionário. Remova uma fruta do dicionário e imprima o dicionário atualizado.
+# Limpe o dicionário, removendo todos os itens.
+# Crie um novo dicionário contendo pelo menos três frutas diferentes e suas quantidades e, em seguida, combine esse dicionário com o primeiro dicionário criado.
