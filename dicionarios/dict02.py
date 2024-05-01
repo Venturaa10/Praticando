0180cc21d04e6b6c7253b850328f008d4bec7dda
Joao Victor Ventura <joao.victor.ventura25@gmail.com>
2024-05-01 18:48:29 -0300

    Proxima leva de exercicios sobre dicionario um pouco mais complexos

---
+++ dicionarios/dict02.py
@@ -0,0 +1,15 @@
+import os
+os.system('cls')
+
+# 1- Contagem de Palavras:
+# Escreva um programa que recebe uma string como entrada e conta a ocorrência de cada palavra na string. Em seguida, armazene essas contagens em um dicionário onde as chaves são as palavras e os valores são as contagens de ocorrências.
+
+
+
+# 2 - Mesclar Dicionários Únicos:
+# Escreva uma função que aceita uma lista de dicionários como entrada e retorna um único dicionário que contém todas as chaves e seus respectivos valores únicos de todos os dicionários de entrada.
+ 
+
+
+# 3 - Média de Notas:
+# Escreva um programa que pede ao usuário para inserir o número de alunos em uma turma, seus nomes e suas notas (em uma escala de 0 a 10) como entrada. Em seguida, calcule e imprima a média das notas. Armazene os nomes dos alunos como chaves e suas notas como valores em um dicionário.
