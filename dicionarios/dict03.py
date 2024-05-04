95419c18c7955fa13d6467e729a82769ff6e11f5
Joao Victor Ventura <joao.victor.ventura25@gmail.com>
2024-05-04 20:10:08 -0300

    Terceiro exercicio concluído

--- dicionarios/dict03.py
+++ dicionarios/dict03.py
@@ -6,7 +6,7 @@ os.system('cls')
 # Crie um dicionário com algumas palavras e suas definições. Peça ao usuário para inserir uma palavra e, em seguida, mostre a definição correspondente. (FEITO)
 dicionario = {'Simples' : 'Facil', 'Pedra' : 'Duro e vem de rocha', 'Planta': 'Verde e leve'}
 
-palavra = input('Digite a palavra: ')
+palavra = input('Digite a palavra: ').capitalize().strip()
 definicao = input(f'Defina a palavra {palavra}: ')
 
 dicionario[palavra] = definicao
@@ -14,6 +14,7 @@ print(dicionario)
 
 # 2- Adicionar e Remover Palavras:
 # Permita que o usuário adicione novas palavras e suas definições ao dicionário. Depois, permita que ele remova palavras existentes, caso deseje.
+'''
 print()
 remover = input('Deseja remover alguma palavra do dicionario, digite "Sim" ou "Não": ').capitalize().strip()
 
@@ -25,12 +26,24 @@ if remover == 'Sim' or remover == 'S':
 
 elif remover == 'Não' or remover == 'Nao':
     pass
+'''
 
 # 3 - Pesquisa de Palavras:
 # Peça ao usuário para inserir uma palavra. Verifique se a palavra está presente no dicionário e, se estiver, mostre sua definição. Caso contrário, exiba uma mensagem indicando que a palavra não foi encontrada.
+'''
+pesquisa_dic = input('Buscar palavra: ').capitalize().strip()
+
+if pesquisa_dic in dicionario:
+    print('Essa palavra já existe no dicionario!')
+    print(f'Chave: {pesquisa_dic}: {dicionario[pesquisa_dic]}')
+
+else:
+    print(f'{pesquisa_dic} não existe no dicionario')
+'''
 
 # 4 - Listar Todas as Palavras:
 # Mostre todas as palavras presentes no dicionário, uma por linha.
+
 
 # 5 - Contagem de Palavras:
 # Conte quantas palavras existem no dicionário e exiba o total para o usuário.
