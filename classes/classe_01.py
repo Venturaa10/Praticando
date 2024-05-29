395e6b0837560399e63f560e412681aa9d2955d8
Joao Victor Ventura <joao.victor.ventura25@gmail.com>
2024-05-28 22:01:09 -0300

    Inicio dos estudos com classes

---
+++ classes/classe_01.py
@@ -0,0 +1,13 @@
+# Exercício 1: Criação de uma Classe Básica
+# Crie uma classe chamada Pessoa que tenha os atributos nome e idade. Implemente um método mostrar_info() que imprime o nome e a idade da pessoa.
+
+# Exercício 2: Adição de um Método para Atualizar a Idade
+# Modifique a classe Pessoa do Exercício 1 para incluir um método atualizar_idade() que permite atualizar a idade da pessoa.
+
+# Exercício 3: Herança de Classes
+# Crie uma classe chamada Aluno que herde da classe Pessoa do Exercício 1. Adicione um atributo adicional chamado matricula e um método mostrar_matricula() que imprime a matrícula do aluno.
+
+# Exercício 4: Sobrescrita de Métodos
+# Na classe Aluno do Exercício 3, sobrescreva o método mostrar_info() para incluir a matrícula do aluno.
+
+# Estes exercícios fornecem uma base sólida para começar a trabalhar com classes em Python, abrangendo desde a criação básica de classes até conceitos mais avançados como herança e sobrescrita de métodos.
