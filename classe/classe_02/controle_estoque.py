9abb8c8794b65f83e4c6496447df702b69c8d347
Joao Victor Ventura <joao.victor.ventura25@gmail.com>
2024-09-29 19:07:47 -0300

    Praticando POO

--- classe/classe_02/controle_estoque.py
+++ classe/classe_02/controle_estoque.py
@@ -1,97 +1,90 @@
 import os
-
 os.system('cls')
-class Produto:
-    produtos = []
+'''Objetivo: Gerenciamento de Estoque
+Você vai criar um sistema simples para gerenciar o estoque de produtos. O sistema deve permitir as seguintes operações:
 
-    def __init__(self, nome: str, preco: float, quantidade: int):
+Criação de Produtos:
+
+Defina uma classe Produto com os atributos:
+nome (string)
+preço (float)
+quantidade (int)
+Métodos da Classe Produto:
+
+adicionar_estoque(quantidade): Adiciona a quantidade especificada ao estoque do produto.
+remover_estoque(quantidade): Remove a quantidade especificada do estoque do produto, se houver estoque suficiente.
+atualizar_preço(novo_preço): Atualiza o preço do produto.
+mostrar_informacoes(): Exibe o nome, preço e quantidade atual do produto.
+Funções Adicionais:
+
+Criar Produtos:
+Crie pelo menos três produtos com nomes, preços e quantidades diferentes.
+Adicionar/Remover Estoque:
+Realize operações de adição e remoção de estoque para esses produtos.
+Atualizar Preços:
+Atualize o preço de pelo menos um dos produtos.
+Exibir Status do Estoque:
+Mostre as informações (nome, preço e quantidade) de todos os produtos.
+O objetivo é implementar e testar essas funcionalidades para gerenciar o estoque de forma eficiente.
+'''
+
+class Produto:
+
+    def __init__(self, nome, preco, quantidade):
+        # Verificação inicial para evitar valores negativos
+        if preco < 0:
+            raise ValueError("Preço não pode ser negativo!")
+        if quantidade < 0:
+            raise ValueError("Quantidade não pode ser negativa!")
+    
         self.nome = nome
         self.preco = preco
         self.quantidade = quantidade
 
-    def adicionar_estoque(self,item):
-        if isinstance(item,Produto):
-            self.produtos.append(item)
+    def adicionar_estoque(self, quantidade):
+        ''' Adiciona uma quantidade fornecida pelo usuario ao estoque'''
+        if quantidade <= 0:
+            raise ValueError('Valor inválido, a quantidade não pode ser menor ou igual a 0!') 
+        else:
+            self.quantidade += quantidade
 
-    def adicionar_qtd(self):
-        # os.system('cls')
-        print('*'*50)
-        print('ATUALIZAR QUANTIDADE EM ESTOQUE')
-        print(f'''
-            Nome do Produto: {self.nome}
-            Qtd. em Estoque: {self.quantidade}    
-''')
-        try:
-            add_quantidade = int(input('Quantidade para adicionar ao estoque: '))
-            if add_quantidade <= 0:
-                print('Não é possivel adicionar um valor negativo ou igual a zero ao estoque!')
-            else:
-                self.quantidade = self.quantidade + add_quantidade
-                print(f'''
-                      Informações do Produto\n
-            Nome do Produto: {self.nome} | Qtd. Estoque: {self.quantidade} | Preço R$:{self.preco:.2f}''')
-        except:
-            print('Não foi possível alterar estoque, verifique a quantidade informada!')
+    def remover_estoque(self, quantidade):
+        ''' Remove determinada quantidade do estoque, se o estoque for maior que 0'''
+        if self.quantidade <= 0:
+            raise ValueError('Estoque insuficiente!')
+        
+        if quantidade > self.quantidade:
+            raise ValueError(f'Quantidade fornecida {quantidade} é maior que a quantidade em estoque {self.quantidade}!')
+        else:
+            self.quantidade -= quantidade
+
+    def atualizar_preco(self, novo_preco):
+        ''' Atualiza o preço do produto'''
+        if novo_preco < 0:
+            raise ValueError('Valor inválido, o preço não pode ser negativo! ')
+        else:
+            self.preco = novo_preco
             
 
-    def remover_estoque(self):
-        # os.system('cls')
-        print('*'*50)
-        print('REMOVER DO ESTOQUE')
-        if self.quantidade >= 1:
-            try:
-                print(f'\nEstoque Atual: {self.quantidade}')
-                remov_quantidade = int(input('Quantidade para remover do estoque: '))
-                if remov_quantidade > self.quantidade:
-                    print(f'\nNão foi possivel realizar a operação!\nQuantidade informada ({remov_quantidade}) é maior que a quantidade em estoque ({self.quantidade})')
-                elif remov_quantidade <= 0:
-                    print('\nO valor não pode ser negativo ou igual a zero!') 
-                else:
-                    self.quantidade = self.quantidade - remov_quantidade
-                    print(f'\nNome do Produto: {self.nome}\nQtd. Estoque Atualizada: {self.quantidade}')
-            except:
-                print('\nA quantidade informada é invalida!')
-        else:
-            print('O estoque atual é negativo ou igual a zero, verificar!')
-            print(f'Produto: {self.nome} | Estoque Atual: {self.quantidade}')
+    def mostrar_informacoes(self):
+        return f'Produto: {self.nome} | Quantidade: {self.quantidade} | Preço: {self.preco:.2f}'
+        
+camisa1 = Produto('Camisa Barcelona', 250, 0)
+camisa1.adicionar_estoque(50)
+camisa1.remover_estoque(25)
+camisa1.atualizar_preco(280)
+print(camisa1.mostrar_informacoes())
 
-    def atualizar_preco(self):
-        # os.system('cls')
-        print('*'*50)
-        print('ATUALIZAÇÃO DE PREÇO')
-        try:
-            print(f'Preço Atual: R${self.preco:.2f}')
-            novo_preco = float(input(f'Novo Preço: '))
-            self.preco = novo_preco
-            print(f'\nNome do Produto: {self.nome}\nPreço Atualizado: R${self.preco:.2f}')
-        except:
-            print('Não foi possivel alterar o preço, verifique a quantidade informada!')
+camisa2 = Produto('Camisa do Manchester City', 200, 75)
+camisa2.adicionar_estoque(70)
+camisa2.remover_estoque(100)
+camisa2.atualizar_preco(230)
+print(camisa2.mostrar_informacoes())
 
-    def exibe_produtos(self):
-        print('Nosso Catalogo')
+camisa3 = Produto('Camisa do Flamengo', 300, 45)
+camisa3.adicionar_estoque(90)
+camisa3.remover_estoque(65)
+camisa3.atualizar_preco(320)
+print(camisa3.mostrar_informacoes())
 
-        for i, item in enumerate(self.produtos):
-            print(f'Nome do Produto: {item.nome} | Qtd. Estoque: {item.quantidade} | Preço: {item.preco}')
-
-
-    def __str__(self):
-        return (f'''
-            INFORMAÇÕES DO PRODUTO\n
-        Nome do Produto: {self.nome}
-        Preço: {self.preco:.2f}
-        Quantidade em Estoque: {self.quantidade}
-        ''')
-
-p1 = Produto('Camisa do Bayern',250,50)
-p2 = Produto('Camisa do Barcelona',280,150)
-p3 = Produto('Camisa do Real Madrid',270,100)
-p4 = Produto('Camisa Teste Estoque Insuficiente',180,0)
-
-
-p1.adicionar_estoque(p1)
-p2.adicionar_estoque(p2)
-p3.adicionar_estoque(p3)
-p4.adicionar_estoque(p4)
-
-p1.exibe_produtos()
 
